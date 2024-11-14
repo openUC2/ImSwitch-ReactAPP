bbd8091c45061e8e889415e79b41e76b7d3fcb15
beniroquai <bene.d@gmx.de>
2024-11-14 13:14:54 +0100

    Update WebSocketContext.js

--- src/context/WebSocketContext.js
+++ src/context/WebSocketContext.js
@@ -6,16 +6,14 @@ const wsport = 8002;
 
 export const WebSocketProvider = ({ hostIP, hostPort, children }) => {
 
+  const [serverUrl, setServerUrl] = useState(`${hostIP}:${wsport}/ws`);
   const [socket, setSocket] = useState(null);
-  const [serverUrl, setServerUrl] = "https://0.0.0.0:8002/ws";
-  
 
   useEffect(() => {
-    const wsUrl = "https://0.0.0.0:8002/ws" ; //`ws://${hostIP.replace(/^https?:\/\//, '')}:${wsport}/ws`;
-    console.log("Connecting to WebSocket at", wsUrl);
-    const ws = new WebSocket(wsUrl);
+    console.log("Connecting to WebSocket at", serverUrl);
+    const ws = new WebSocket(serverUrl);
 
-    ws.onopen = () => console.log("WebSocket connected to", wsUrl);
+    ws.onopen = () => console.log("WebSocket connected to", serverUrl);
     ws.onmessage = (event) => console.log("Message received:", event.data);
     ws.onclose = () => console.log("WebSocket disconnected.");
     ws.onerror = (error) => console.error("WebSocket error:", error);
