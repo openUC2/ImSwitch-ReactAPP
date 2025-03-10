c63aa7b45567bb93858603ecb75f5fbaeef46e52
Marco Franke <marco.franke@axon-innovations.com>
2025-03-10 14:19:58 +0100

    remove hard coded api calls

--- src/axon/PositionControllerComponent.js
+++ src/axon/PositionControllerComponent.js
@@ -11,63 +11,138 @@ import {
   Box,
 } from "@mui/material";
 
+import apiPositionerControllerMovePositioner from "../backendapi/apiPositionerControllerMovePositioner.js";
+
 //##################################################################################
 const PositionControllerComponent = () => {
   const [intervalId, setIntervalId] = useState(null);
 
+  const moveDistance = 5000;//TODO adjust
+  const zoomDistance = 100;//TODO adjust
+
   //##################################################################################
-  const movePosition = (direction) => {
-    const url = `https://imswitch.openuc2.com/PositionerController/movePositioner?positionerName=VirtualStage&speed=20000&axis=`;
-
-    let axis, dist;
-
-    const speed = 5000;
-
-    switch (direction) {
-      case "up":
-        axis = "Y";
-        dist = speed;
-        break;
-      case "down":
-        axis = "Y";
-        dist = -speed;
-        break;
-      case "left":
-        axis = "X";
-        dist = speed;
-        break;
-      case "right":
-        axis = "X";
-        dist = -speed;
-        break;
-      default:
-        return;
-    }
-
-    const apiUrl = `${url}${axis}&dist=${dist}&isAbsolute=false&isBlocking=false&speed=20000`;
-
-    // Send the request to the server
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Moved Successfully:", data);
+  const moveUp = () => {
+    apiPositionerControllerMovePositioner({
+      axis: "Y",
+      dist: moveDistance,
+      isAbsolute: false,
+    })
+      .then((positionerResponse) => {
+        console.log("Moved successfully:", positionerResponse);
       })
       .catch((error) => {
-        console.error("Moved Error:", error);
+        console.log("Moved error:", error);
       });
   };
 
   //##################################################################################
-  const startMoving = (direction) => {
+  const moveDown = () => {
+    apiPositionerControllerMovePositioner({
+      axis: "Y",
+      dist: -moveDistance,
+      isAbsolute: false,
+    })
+      .then((positionerResponse) => {
+        console.log("Moved successfully:", positionerResponse);
+      })
+      .catch((error) => {
+        console.log("Moved error:", error);
+      });
+  };
+
+  //##################################################################################
+  const moveLeft = () => {
+    apiPositionerControllerMovePositioner({
+      axis: "X",
+      dist: moveDistance,
+      isAbsolute: false,
+    })
+      .then((positionerResponse) => {
+        console.log("Moved successfully:", positionerResponse);
+      })
+      .catch((error) => {
+        console.log("Moved error:", error);
+      });
+  };
+
+  //##################################################################################
+  const moveRight = () => {
+    apiPositionerControllerMovePositioner({
+      axis: "X",
+      dist: -moveDistance,
+      isAbsolute: false,
+    })
+      .then((positionerResponse) => {
+        console.log("Moved successfully:", positionerResponse);
+      })
+      .catch((error) => {
+        console.log("Moved error:", error);
+      });
+  };
+
+  //##################################################################################
+  const zoomPlus = () => {
+    apiPositionerControllerMovePositioner({
+      axis: "Z",
+      dist: zoomDistance,
+      isAbsolute: false,
+    })
+      .then((positionerResponse) => {
+        console.log("Zoom successfully:", positionerResponse);
+      })
+      .catch((error) => {
+        console.log("Zoom error:", error);
+      });
+  };
+
+  //##################################################################################
+  const zoomMinus = () => {
+    apiPositionerControllerMovePositioner({
+      axis: "Z",
+      dist: -zoomDistance,
+      isAbsolute: false,
+    })
+      .then((positionerResponse) => {
+        console.log("Zoom successfully:", positionerResponse);
+      })
+      .catch((error) => {
+        console.log("Zoom error:", error);
+      });
+  };
+
+  //##################################################################################
+  const startPeriodicRequest = (action) => {
     // Start calling movePosition repeatedly
     const id = setInterval(() => {
-      movePosition(direction);
+      switch (action) {
+        case "minus":
+          zoomMinus();
+          break;
+        case "plus":
+          zoomPlus();
+          break;
+        case "up":
+          moveUp();
+          break;
+        case "down":
+          moveDown();
+          break;
+        case "left":
+          moveLeft();
+          break;
+        case "right":
+          moveRight();
+          break;
+        default:
+            console.log("ERROR unhandled action:", action);
+          break;
+      }
     }, 100); // 100ms interval, adjust as needed
     setIntervalId(id);
   };
 
   //##################################################################################
-  const stopMoving = () => {
+  const stopPeriodicRequest = () => {
     // Clear the interval to stop calling movePosition
     if (intervalId) {
       clearInterval(intervalId);
@@ -82,58 +157,60 @@ const PositionControllerComponent = () => {
       style={{
         padding: "10px",
         border: "0px solid white",
-        display: 'flex', flexDirection: 'column', gap: '4px'
+        display: "flex",
+        flexDirection: "column",
+        gap: "4px",
       }}
     >
-      <div style={{  display: 'flex', gap: '4px' }}>
+      <div style={{ display: "flex", gap: "4px" }}>
         <Button
-         variant="contained"
-          onMouseDown={() => startMoving("minus")}
-          onMouseUp={stopMoving}
-          onMouseLeave={stopMoving}
+          variant="contained"
+          onMouseDown={() => startPeriodicRequest("minus")}
+          onMouseUp={stopPeriodicRequest}
+          onMouseLeave={stopPeriodicRequest}
         >
           -
         </Button>
         <Button
-         variant="contained"
-          onMouseDown={() => startMoving("up")}
-          onMouseUp={stopMoving}
-          onMouseLeave={stopMoving} 
+          variant="contained"
+          onMouseDown={() => startPeriodicRequest("up")}
+          onMouseUp={stopPeriodicRequest}
+          onMouseLeave={stopPeriodicRequest}
         >
           ↑
         </Button>
         <Button
-         variant="contained"
-          onMouseDown={() => startMoving("plus")}
-          onMouseUp={stopMoving}
-          onMouseLeave={stopMoving} 
+          variant="contained"
+          onMouseDown={() => startPeriodicRequest("plus")}
+          onMouseUp={stopPeriodicRequest}
+          onMouseLeave={stopPeriodicRequest}
         >
           +
         </Button>
       </div>
 
-      <div style={{  display: 'flex', gap: '4px' }}>
+      <div style={{ display: "flex", gap: "4px" }}>
         <Button
-         variant="contained"
-          onMouseDown={() => startMoving("left")}
-          onMouseUp={stopMoving}
-          onMouseLeave={stopMoving} 
+          variant="contained"
+          onMouseDown={() => startPeriodicRequest("left")}
+          onMouseUp={stopPeriodicRequest}
+          onMouseLeave={stopPeriodicRequest}
         >
           ←
         </Button>
         <Button
-         variant="contained"
-          onMouseDown={() => startMoving("down")}
-          onMouseUp={stopMoving}
-          onMouseLeave={stopMoving} 
+          variant="contained"
+          onMouseDown={() => startPeriodicRequest("down")}
+          onMouseUp={stopPeriodicRequest}
+          onMouseLeave={stopPeriodicRequest}
         >
           ↓
         </Button>
         <Button
-         variant="contained"
-          onMouseDown={() => startMoving("right")}
-          onMouseUp={stopMoving}
-          onMouseLeave={stopMoving} 
+          variant="contained"
+          onMouseDown={() => startPeriodicRequest("right")}
+          onMouseUp={stopPeriodicRequest}
+          onMouseLeave={stopPeriodicRequest}
         >
           →
         </Button>
