e468cc2571390296b0568a4a68e3103732eb4c1a
beniroquai <bene.d@gmx.de>
2025-10-22 06:34:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import DetectorTriggerController from "./components/DetectorTriggerController.js
 import ExtendedLEDMatrixController from "./components/ExtendedLEDMatrixController.js";
 import FlowStopController from "./components/FlowStopController.js";
 import FocusLockController from "./components/FocusLockController.js";
-import HistoScanController from "./components/HistoScanController.js";
 import ImJoyView from "./components/ImJoyView.js";
 import JupyterExecutor from "./components/JupyterExecutor.js";
 import LepMonController from "./components/LepmonController.js";
@@ -447,7 +446,6 @@ function App() {
             {selectedPlugin === "ImJoy" && (
               <ImJoyView sharedImage={sharedImage} />
             )}
-            {selectedPlugin === "HistoScan" && <HistoScanController />}
             {selectedPlugin === "STORMLocal" && <STORMControllerLocal />}
             {selectedPlugin === "STORMArkitekt" && <STORMControllerArkitekt />}
             {selectedPlugin === "Stresstest" && <StresstestController />}
