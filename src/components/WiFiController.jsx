bf3e993e8e0db716b7024b0838eaa3f27470abf8
Florian Paproth <florian@paproth.biz>
2025-11-28 00:55:15 +0100

    refactor: update WiFiController to handle iframe errors and improve error messaging; remove unused WiFiSlice

--- src/components/WiFiController.jsx
+++ src/components/WiFiController.jsx
@@ -1,10 +1,13 @@
 // src/components/WiFiController.js
 import { Alert, Box, Button, Paper, Typography } from "@mui/material";
 import { OpenInNew as OpenInNewIcon } from "@mui/icons-material";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import * as connectionSettingsSlice from "../state/slices/ConnectionSettingsSlice.js";
 
 const WiFiController = () => {
+  const [iframeError, setIframeError] = useState(false);
+
   // Access ImSwitch backend connection settings from Redux - following Copilot Instructions
   const connectionSettingsState = useSelector(
     connectionSettingsSlice.getConnectionSettingsState
@@ -15,6 +18,10 @@ const WiFiController = () => {
   // According to developer: if ImSwitch URL is {protocol}://{hostname}:8001/imswitch/index.html
   // then device-admin Internet Access page is {protocol}://{hostname}/admin/panel/internet
   const internetAccessUrl = `${hostIP}/admin/panel/internet`;
+
+  const handleIframeError = () => {
+    setIframeError(true);
+  };
 
   return (
     <Paper sx={{ p: 2 }}>
@@ -42,6 +49,21 @@ const WiFiController = () => {
         </Button>
       </Alert>
 
+      {iframeError && (
+        <Alert severity="warning" sx={{ mb: 2 }}>
+          The admin panel could not be loaded in the iframe. This may be due to
+          network issues or browser security restrictions.
+          <Button
+            size="small"
+            startIcon={<OpenInNewIcon />}
+            onClick={() => window.open(internetAccessUrl, "_blank")}
+            sx={{ ml: 1 }}
+          >
+            Open in new tab
+          </Button>
+        </Alert>
+      )}
+
       <Box
         sx={{
           width: "100%",
@@ -50,18 +72,51 @@ const WiFiController = () => {
           borderColor: "divider",
           borderRadius: 1,
           overflow: "hidden",
+          position: "relative",
         }}
       >
         <iframe
           src={internetAccessUrl}
           title="Internet Access Configuration"
-          sandbox="allow-same-origin allow-scripts allow-forms allow-popups"
+          onError={handleIframeError}
           style={{
             width: "100%",
             height: "100%",
             border: "none",
           }}
         />
+        {iframeError && (
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              bgcolor: "background.default",
+              flexDirection: "column",
+              gap: 2,
+              p: 4,
+            }}
+          >
+            <Typography variant="h6" color="text.secondary">
+              Unable to load admin panel
+            </Typography>
+            <Typography variant="body2" color="text.secondary" align="center">
+              The page at <code>{internetAccessUrl}</code> could not be loaded.
+            </Typography>
+            <Button
+              variant="contained"
+              startIcon={<OpenInNewIcon />}
+              onClick={() => window.open(internetAccessUrl, "_blank")}
+            >
+              Open in New Tab
+            </Button>
+          </Box>
+        )}
       </Box>
     </Paper>
   );
